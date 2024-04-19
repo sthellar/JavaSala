ea4fa4999c7a8b5c3abdfa8cf526091934760833
Sthellar <estherbtsilva@gmail.com>
2024-04-18 21:35:30 -0300

    Adicionar a tarefa.

---
+++ nomecompleto.js
@@ -0,0 +1,6 @@
+let nome = prompt ("Digite seu nome");
+let sobrenome = prompt ("Digite seu sobrenome"); 
+
+alert ("Seu nome completo" + nome + sobrenome); 
+alert (sobrenome.toUpperCase() + "," + nome);
+
